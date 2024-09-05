a9fb8c537e9f09882989217b239bb6b26e127c69
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-09-06 00:20:59 +0800

    show hardcoded messages

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const app = express()
+const path = require("node:path")
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 const port = 3000
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
 
 app.get('/', (req, res) => {
+    res.render("index", { title: "Mini Messageboard", messages: messages })
+})
+
+app.get('/new', (req, res) => {
     res.send('hello world')
 })
+
+app.post('/new', (req, res) => {
+    res.send('hello world')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
